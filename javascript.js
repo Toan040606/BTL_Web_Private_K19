fba6c8968ac8fdf0744fabf1fa7907703ceee174
Toan040606 <toannickson2k60604@gmail.com>
2026-02-27 14:52:08 +0700

    Refactor: update series navigation links

--- javascript.js
+++ javascript.js
@@ -1,3 +1,344 @@
-document.addEventListener("DOMContentLoaded", () => {
-    window.location.href = `./pages/home/index.html`;
-})
+let home = [];
+let homeMovies = [];
+let homeImgsLink = [];
+let actionMovies = [];
+let chineseMovies = [];
+let animeMovies = [];
+let imgLink;
+
+async function loadMovies() {
+  const res = await fetch("https://ophim1.com/v1/api/home");
+  const data = await res.json();
+  home = data;
+  imgLink = data.data.APP_DOMAIN_CDN_IMAGE;
+  homeMovies = data.data.items;
+  homeImgsLink = data.data.seoOnPage.og_image;
+}
+
+async function loadActionMovies() {
+  const res = await fetch("https://ophim1.com/v1/api/danh-sach/hanh-dong");
+  const data = await res.json();
+  actionMovies = data.data.items;
+}
+
+async function loadChineseMovies() {
+  const res = await fetch(
+    "https://ophim1.com/v1/api/danh-sach/phim-moi?country=trung-quoc",
+  );
+  const data = await res.json();
+  console.log(data);
+  chineseMovies = data.data.items;
+}
+
+async function loadAnimeMovies() {
+  const res = await fetch("https://ophim1.com/v1/api/danh-sach/hoat-hinh");
+  const data = await res.json();
+  animeMovies = data.data.items;
+}
+
+const fomatPosterUrl = (url) => {
+  if (url.includes("thumb")) {
+    return url.replace("thumb", "poster");
+  }
+  return url;
+};
+
+const is18Plus = (movie) => {
+  return movie.category.some((category) => category.slug === "phim-18");
+};
+
+const isThaiLand = (movie) => {
+  return movie.country.some((country) => country.slug === "thai-lan");
+};
+
+const removeSensitiveFilms = (list) => {
+  return list.filter(
+    (film) => !is18Plus(film) && !isThaiLand(film)
+  );
+};
+
+const renderSlider = (imgLink, homeMovies) => {
+  const sliderContainer = document.querySelectorAll(".slider .item");
+
+  for (let i = 0; i < 10; i++) {
+    const img = document.createElement("img");
+    img.src = `${imgLink}/uploads/movies/${fomatPosterUrl(homeMovies[i].thumb_url)}`;
+    img.alt = `${homeMovies[i].name}`;
+    sliderContainer[i].appendChild(img);
+  }
+
+  sliderContainer[0].classList.add("active");
+};
+
+const renderContent = (homeMovies) => {
+  const contentContainer = document.querySelectorAll(".content-slider .item");
+  for (let i = 0; i < 10; i++) {
+    contentContainer[i].innerHTML = "";
+  }
+  for (let i = 0; i < 10; i++) {
+    contentContainer[i].innerHTML += `
+    <div class="item-content">
+        <h3>${homeMovies[i].name}</h3>
+        <p>${homeMovies[i].origin_name}</p>
+        <div class="buttons">
+          <button class="play-btn" onclick="goToWatchPage('${homeMovies[i].slug}')"><i class="fa-solid fa-play"></i> <span>Xem ngay</span></button>
+          <button class="favourite-btn"><i class="fa-regular fa-heart"></i></button>
+          <button class="share-btn">
+            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="text-[24px]" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="128" cy="256" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle>
+              <circle cx="384" cy="112" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle>
+              <circle cx="384" cy="400" r="48" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"></circle>
+              <path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="m169.83 279.53 172.34 96.94m0-240.94-172.34 96.94"></path>
+            </svg>
+          </button>
+        </div>
+    </div>
+    `;
+  }
+  contentContainer[0].classList.add("active");
+};
+
+const renderImgs = (imgLink, homeImgsLink) => {
+  const sliderContainer = document.querySelectorAll(".thumb-slider .item");
+
+  for (let i = 0; i < 10; i++) {
+    sliderContainer[i].innerHTML = "";
+  }
+
+  for (let i = 0; i < 10; i++) {
+    sliderContainer[i].innerHTML += `
+    <img src="${imgLink}/uploads/movies/${fomatPosterUrl(homeMovies[i].thumb_url)}" alt="Thumbnail ${i + 1}" />
+    `;
+  }
+
+  sliderContainer[0].classList.add("active");
+};
+
+let setActive = 1;
+
+const autoNext = () => {
+  let itemSlider = document.querySelectorAll(".slider .item");
+  let contentSlider = document.querySelectorAll(".content-slider .item");
+  let thumbSlider = document.querySelectorAll(".thumb-slider .item");
+  let thumbSliderContainer = document.querySelector(".thumb-slider");
+
+  setInterval(async () => {
+    if (setActive >= 10) {
+      setActive = 0;
+      itemSlider[9].classList.remove("active");
+      contentSlider[9].classList.remove("active");
+      thumbSlider[9].classList.remove("active");
+
+      itemSlider[setActive].classList.add("active");
+      contentSlider[setActive].classList.add("active");
+      thumbSlider[setActive].classList.add("active");
+    } else {
+      itemSlider[setActive - 1].classList.remove("active");
+      contentSlider[setActive - 1].classList.remove("active");
+      thumbSlider[setActive - 1].classList.remove("active");
+
+      itemSlider[setActive].classList.add("active");
+      contentSlider[setActive].classList.add("active");
+      thumbSlider[setActive].classList.add("active");
+    }
+
+    /*
+    lấy khoảng cách của item cần di chuyển cách mép trái container bao nhiêu px
+    rồi trừ đi nửa chiều rộng container để item đó canh item ra giữa nhưng sẽ bị lệch về bên phải
+    sau đó cộng với nửa chiều rộng item để nó không bị lệch phải nữa
+    omg làm sao tôi nghĩ ra đc cái này nhỉ ???? :vvvvv
+    */
+    const scrollPosition = thumbSlider[setActive].offsetLeft -
+      thumbSliderContainer.offsetWidth / 2 +
+      thumbSlider[setActive].offsetWidth / 2;
+    thumbSliderContainer.scrollTo({
+      left: scrollPosition,
+      behavior: "smooth",
+    });
+
+    setActive++;
+  }, 20000);
+};
+
+const clickThumb = () => {
+  let itemSlider = document.querySelectorAll(".slider .item");
+  let contentSlider = document.querySelectorAll(".content-slider .item");
+  let thumbSlider = document.querySelectorAll(".thumb-slider .item");
+  let thumbSliderContainer = document.querySelector(".thumb-slider");
+
+  thumbSlider.forEach((thumb, index) => {
+    thumb.addEventListener("click", () => {
+      console.log("Clicked thumb index:", setActive);
+      document
+        .querySelector(".slider .item.active")
+        .classList.remove("active");
+      document
+        .querySelector(".content-slider .item.active")
+        .classList.remove("active");
+      document
+        .querySelector(".thumb-slider .item.active")
+        .classList.remove("active");
+
+      itemSlider[index].classList.add("active");
+      contentSlider[index].classList.add("active");
+      thumbSlider[index].classList.add("active");
+
+      const scrollPosition =
+        thumbSlider[index].offsetLeft -
+        thumbSliderContainer.offsetWidth / 2 +
+        thumbSlider[index].offsetWidth / 2;
+      thumbSliderContainer.scrollTo({
+        left: scrollPosition,
+        behavior: "smooth",
+      });
+
+      setActive = index + 1;
+    });
+  });
+};
+
+const renderThinhHanh = (homeMovies) => {
+  const thinhHanhContainer = document.querySelector(".thinh-hanh .movie-list");
+  thinhHanhContainer.innerHTML = "";
+
+  homeMovies.forEach((movie) => {
+    thinhHanhContainer.innerHTML += `
+    <div class="movie-item" onclick="goToWatchPage('${movie.slug}')">
+      <img src="https://img.ophim1.com/uploads/movies/${movie.thumb_url}" alt="${movie.name}" />
+      <h4>${movie.name}</h4>
+      <div class="movie-card">
+        <img src="https://img.ophim1.com/uploads/movies/${fomatPosterUrl(movie.thumb_url)}" alt="${movie.name}" />
+        <h4>${movie.name}</h4>
+        <p>${movie.origin_name}</p>
+        <div class="buttons">
+          <button class="play-btn" onclick="goToWatchPage('${movie.slug}')"><i class="fa-solid fa-play"></i> <span>Xem ngay</span></button>
+          <button class="favourite-btn"><i class="fa-regular fa-heart"></i></button>
+        </div>
+      </div>
+    </div>
+  `;
+  });
+};
+
+const renderAnime = (animeMovies) => {
+  const phimVietContainer = document.querySelector(".anime .movie-list");
+  phimVietContainer.innerHTML = "";
+
+  animeMovies.forEach((movie) => {
+    phimVietContainer.innerHTML += `
+    <div class="movie-item" onclick="goToWatchPage('${movie.slug}')">
+      <img src="https://img.ophim1.com/uploads/movies/${movie.thumb_url}" alt="${movie.name}" />
+      <h4>${movie.name}</h4>
+      <div class="movie-card">
+        <img src="https://img.ophim1.com/uploads/movies/${fomatPosterUrl(movie.thumb_url)}" alt="${movie.name}" />
+        <h4>${movie.name}</h4>
+        <p>${movie.origin_name}</p>
+        <div class="buttons">
+          <button class="play-btn" onclick="goToWatchPage('${movie.slug}')"><i class="fa-solid fa-play"></i> <span>Xem ngay</span></button>
+          <button class="favourite-btn"><i class="fa-regular fa-heart"></i></button>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+};
+
+const renderAction = (actionMovies) => {
+  const actionContainer = document.querySelector(".hanh-dong .movie-list");
+  actionContainer.innerHTML = "";
+
+  actionMovies.forEach((movie) => {
+    actionContainer.innerHTML += `
+      <div class="movie-item" onclick="goToWatchPage('${movie.slug}')">
+        <img src="https://img.ophim1.com/uploads/movies/${movie.thumb_url}" alt="${movie.name}" />
+        <h4>${movie.name}</h4>
+        <div class="movie-card">
+        <img src="https://img.ophim1.com/uploads/movies/${fomatPosterUrl(movie.thumb_url)}" alt="${movie.name}" />
+        <h4>${movie.name}</h4>
+        <p>${movie.origin_name}</p>
+        <div class="buttons">
+          <button class="play-btn" onclick="goToWatchPage('${movie.slug}')"><i class="fa-solid fa-play"></i> <span>Xem ngay</span></button>
+          <button class="favourite-btn"><i class="fa-regular fa-heart"></i></button>
+        </div>
+      </div>
+      </div>
+      `;
+  });
+};
+
+const renderChinese = (chineseMovies) => {
+  const chineseContainer = document.querySelector(".phim-trung .movie-list");
+  chineseContainer.innerHTML = "";
+
+  chineseMovies.forEach((movie) => {
+    chineseContainer.innerHTML += `
+    <div class="movie-item" onclick="goToWatchPage('${movie.slug}')">
+      <img src="https://img.ophim1.com/uploads/movies/${movie.thumb_url}" alt="${movie.name}" />
+      <h4>${movie.name}</h4>
+      <div class="movie-card">
+        <img src="https://img.ophim1.com/uploads/movies/${fomatPosterUrl(movie.thumb_url)}" alt="${movie.name}" />
+        <h4>${movie.name}</h4>
+        <p>${movie.origin_name}</p>
+        <div class="buttons">
+          <button class="play-btn" onclick="goToWatchPage('${movie.slug}')"><i class="fa-solid fa-play"></i> <span>Xem ngay</span></button>
+          <button class="favourite-btn"><i class="fa-regular fa-heart"></i></button>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+};
+
+async function allCategory() {
+  const res = await fetch("https://ophim1.com/v1/api/quoc-gia");
+  const data = await res.json();
+  console.log(data);
+}
+
+const goToWatchPage = (slug) => {
+  window.location.href = `../watch/index.html?name=${slug}`;
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadMovies();
+  await loadActionMovies();
+  await allCategory();
+  await loadChineseMovies();
+  await loadAnimeMovies();
+
+  setTimeout(() => {
+    document.getElementById("intro").classList.add("fade-out");
+  }, 2000);
+
+  console.log(imgLink);
+  console.log(home);
+  console.log(homeMovies);
+  console.log(homeImgsLink);
+  console.log(actionMovies);
+  console.log(animeMovies);
+  console.log(chineseMovies);
+
+  homeMovies = removeSensitiveFilms(homeMovies);
+  animeMovies = removeSensitiveFilms(animeMovies);
+  actionMovies = removeSensitiveFilms(actionMovies);
+  chineseMovies = removeSensitiveFilms(chineseMovies);
+
+  console.log(homeMovies);
+  console.log(actionMovies);
+  console.log(animeMovies);
+  console.log(chineseMovies);
+
+  renderSlider(imgLink, homeMovies);
+  renderContent(homeMovies);
+  renderImgs(imgLink, homeMovies);
+  clickThumb();
+  autoNext();
+
+  renderThinhHanh(homeMovies);
+
+  renderAnime(animeMovies);
+
+  renderAction(actionMovies);
+
+  renderChinese(chineseMovies);
+});
